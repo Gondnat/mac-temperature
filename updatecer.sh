4a8380d5a2adfc6f05710930baf6f96b0e4819d1
Gondnat <julykitt@gmail.com>
2018-08-29 21:20:47 +0800

    add script

---
+++ updatecer.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+export CERTIFICATE_P12=Certificate.p12;
+echo $CERTIFICATE_OSX_P12 | base64 --decode > $CERTIFICATE_P12;
+export KEYCHAIN=build.keychain;
+security create-keychain -p mysecretpassword $KEYCHAIN;
+security default-keychain -s $KEYCHAIN;
+security unlock-keychain -p mysecretpassword $KEYCHAIN;
+security import $CERTIFICATE_P12 -k $KEYCHAIN -P $CERTIFICATE_PASSWORD -T /usr/bin/codesign;
+security set-key-partition-list -S apple-tool:,apple: -s -k mysecretpassword $KEYCHAIN;
